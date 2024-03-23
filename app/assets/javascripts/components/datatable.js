151bf2a14db687bad826a87765f4e00ce872cf35
Alef Ojeda de Oliveira <alef.oliveira@siedos.com.br>
2024-03-23 13:36:38 -0300

    remove fade class of loading in datatable

--- app/assets/javascripts/components/datatable.js
+++ app/assets/javascripts/components/datatable.js
@@ -38,7 +38,7 @@ class Datatable {
     return `
       <tr>
         <td colspan="6" class="text-center">
-        <div class="d-flex align-items-center fade">
+        <div class="d-flex align-items-center">
           <strong>Loading...</strong>
           <div class="spinner-border ms-auto" role="status" aria-hidden="true"></div>
         </div>
