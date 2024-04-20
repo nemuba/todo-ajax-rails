8aa2bef8c9bdfa92693e6a5f132a6987de2b64ba
Alef Ojeda de Oliveira <alef.oliveira@siedos.com.br>
2024-04-20 20:52:06 -0300

    Adicionar testes para o modelo Todo

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -13,16 +13,27 @@ RSpec.describe Todo, type: :model do
     it { should validate_presence_of(:status) }
   end
 
+  describe 'enums' do
+    it { should define_enum_for(:status).with_values(pending: 0, completed: 1) }
+  end
+
   describe 'attributes' do
     it { should respond_to(:field) }
     it { should respond_to(:inline_editing) }
   end
 
-  describe 'methods' do
-    it { should respond_to(:turbo_stream_append) }
-    it { should respond_to(:turbo_stream_prepend) }
-    it { should respond_to(:turbo_stream_replace) }
-    it { should respond_to(:turbo_stream_remove) }
-    it { should respond_to(:turbo_stream_inline) }
+  describe 'includes' do
+    describe 'Broadcaster' do
+      it { expect(Todo.included_modules).to include(Broadcaster) }
+      it { expect(Todo).to respond_to(:broadcast_to) }
+
+      describe 'methods' do
+        it { should respond_to(:turbo_stream_append) }
+        it { should respond_to(:turbo_stream_prepend) }
+        it { should respond_to(:turbo_stream_replace) }
+        it { should respond_to(:turbo_stream_remove) }
+        it { should respond_to(:turbo_stream_inline) }
+      end
+    end
   end
 end
