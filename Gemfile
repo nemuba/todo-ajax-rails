5f61c2c14d76d92ed1a4f3bd786d7312478793b9
Alef ojeda de Oliveira <alef.oliveira@siedos.com.br>
2025-10-28 16:08:40 -0300

    feat(env): add dotenv-rails for environment variable management and update database configurations

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'uglifier', '>= 1.3.0'
 
 group :development, :test do
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
+  gem 'dotenv-rails'
   gem 'rubocop-rails', require: false
 
   gem 'database_cleaner'
--- config/application.rb
+++ config/application.rb
@@ -14,10 +14,12 @@ require 'action_view/railtie'
 require 'action_cable/engine'
 require 'sprockets/railtie'
 # require "rails/test_unit/railtie"
-
+require 'dotenv/load'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
+Dotenv.load(".env.#{Rails.env}")
 
 module TodoAjaxRails
   class Application < Rails::Application
